f012588ff0521690c526d9f42a424e600dd1ee60
Ariel Mahler <arielmahler18@gmail.com>
2018-08-05 00:27:54 -0700

    This is ****

---
+++ src/rootPackage/Main.java
@@ -0,0 +1,14 @@
+package rootPackage;
+
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("You are about to embark on a journey of a lifetime...");
+		System.out.println("But first, I want to know something:");
+		System.out.println("What kind of fighter are you?");
+		
+	}
+}
